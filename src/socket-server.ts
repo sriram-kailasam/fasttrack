0d8f599c64acd3625e7ac192c59e380e31d772ca
Sriram Kailasam <sriramklash@gmail.com>
2019-10-20 16:06:17 +0530

    Untested hospital id

--- src/socket-server.ts
+++ src/socket-server.ts
@@ -4,17 +4,31 @@ import { Db } from "mongodb";
 import crypto from "crypto";
 
 export function configSocket(socketServer: Server, redis: Redis, db: Db) {
-  socketServer.on("connect", function(socket: Socket) {
+  socketServer.on("connect", async function(socket: Socket) {
     console.log("User connected");
-    const hospitalId = crypto.randomBytes(20).toString("hex");
+
+    const hospital = await db.collection("hospitals").findOne({});
+    const hospitalId = hospital.id;
+
     socket.emit("message", hospitalId);
+    this.redis.sadd("hospitals", hospitalId);
 
     socket.on("admit-request-accept", async data => {
       if (
         await redis.sismember("pending-admit-requests", data.admitRequestId)
       ) {
         redis.srem("pending-admit-requests", data.admitRequestId);
-        socketServer.sockets.emit("admit-request-accept", data.admitRequestId);
+
+        const hospital = await db
+          .collection("hospitals")
+          .findOne({ id: hospitalId });
+
+        socketServer.sockets.emit("admit-request-accept", {
+          location: hospital.location,
+          hospitalId,
+          admitRequestId: data.admitRequestId
+        });
+
         db.collection("admit-requests").updateOne(
           { id: data.admitRequestId },
           {
